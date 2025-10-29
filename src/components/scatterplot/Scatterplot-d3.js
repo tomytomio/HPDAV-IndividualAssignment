0c16b60bb5f5a30b660851b8834e227e657437f7
Lon Daue <tom.gave27@gmail.com>
2025-10-29 17:55:51 +0100

    Initial commit

--- src/components/scatterplot/Scatterplot-d3.js
+++ src/components/scatterplot/Scatterplot-d3.js
@@ -1,5 +1,4 @@
-import * as d3 from 'd3'
-// import { getDefaultFontSize } from '../../utils/helper';
+import * as d3 from "d3";
 
 class ScatterplotD3 {
     margin = {top: 100, right: 10, bottom: 50, left: 100};
@@ -7,146 +6,179 @@ class ScatterplotD3 {
     height;
     width;
     matSvg;
-    // add specific class properties used for the vis render/updates
-    defaultOpacity=0.3;
-    transitionDuration=1000;
+    defaultOpacity = 0.3;
+    transitionDuration = 300;
     circleRadius = 3;
     xScale;
     yScale;
-
+    pointsG;
+    brushG;
+    data = [];
 
     constructor(el){
-        this.el=el;
+        this.el = el;
     };
 
     create = function (config) {
         this.size = {width: config.size.width, height: config.size.height};
 
-        // get the effect size of the view by subtracting the margin
         this.width = this.size.width - this.margin.left - this.margin.right;
         this.height = this.size.height - this.margin.top - this.margin.bottom ;
-        console.log("create SVG width=" + (this.width + this.margin.left + this.margin.right ) + " height=" + (this.height+ this.margin.top + this.margin.bottom));
-        // initialize the svg and keep it in a class property to reuse it in renderScatterplot()
-        this.matSvg=d3.select(this.el).append("svg")
+        // remove previous content
+        d3.select(this.el).selectAll("*").remove();
+
+        this.matSvg = d3.select(this.el).append("svg")
             .attr("width", this.width + this.margin.left + this.margin.right)
             .attr("height", this.height + this.margin.top + this.margin.bottom)
             .append("g")
             .attr("class","matSvgG")
             .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");
-        ;
 
         this.xScale = d3.scaleLinear().range([0,this.width]);
         this.yScale = d3.scaleLinear().range([this.height,0]);
 
-        // build xAxisG
         this.matSvg.append("g")
             .attr("class","xAxisG")
-            .attr("transform","translate(0,"+this.height+")")
-        ;
+            .attr("transform","translate(0,"+this.height+")");
+
         this.matSvg.append("g")
-            .attr("class","yAxisG")
-        ;
+            .attr("class","yAxisG");
+
+        // points group (all markers) and brush layer
+        this.pointsG = this.matSvg.append("g").attr("class","pointsG");
+        this.brushG = this.matSvg.append("g").attr("class","brushG");
     }
 
     changeBorderAndOpacity(selection, selected){
-        selection.style("opacity", selected?1:this.defaultOpacity)
-        ;
-
+        selection.style("opacity", selected ? 1 : this.defaultOpacity);
         selection.select(".markerCircle")
-            .attr("stroke-width",selected?2:0)
-        ;
+            .attr("stroke-width", selected ? 2 : 0);
     }
 
-    updateMarkers(selection,xAttribute,yAttribute){
-        // transform selection
+    updateMarkers(selection, xAttribute, yAttribute){
         selection
             .transition().duration(this.transitionDuration)
             .attr("transform", (item)=>{
-                // use scales to return shape position from data values
                 const xPos = this.xScale(item[xAttribute]);
                 const yPos = this.yScale(item[yAttribute]);
                 return "translate("+xPos+","+yPos+")";
-            })
-        ;
-        this.changeBorderAndOpacity(selection,false)
+            });
+        this.changeBorderAndOpacity(selection, false);
     }
 
     highlightSelectedItems(selectedItems){
-        // use pattern update to change the border and opacity of the markers:
-        //      - call this.changeBorderAndOpacity(selection,true) for markers that match selectedItems
-        //      - this.changeBorderAndOpacity(selection,false) for markers the do not match selectedItems
-        this.matSvg.selectAll(".markerG")
-            // all elements with the class .markerG (empty the first time)
-            .data(selectedItems,(itemData)=>itemData.index)
-            .join(
-                enter=>enter,
-                update=>{
-                    this.changeBorderAndOpacity(update, true);
-                },
-                exit => {
-                    this.changeBorderAndOpacity(exit, false);
-                }
-            )
-        ;
+        const selectedSet = new Set((selectedItems || []).map(d => d.index));
+        this.pointsG.selectAll(".markerG")
+            .select(".markerCircle")
+            .attr("fill", d => selectedSet.has(d.index) ? "orange" : "steelblue")
+            .attr("r", d => selectedSet.has(d.index) ? this.circleRadius + 1.5 : this.circleRadius);
+
+        this.pointsG.selectAll(".markerG")
+            .style("opacity", d => selectedSet.size === 0 ? this.defaultOpacity : (selectedSet.has(d.index) ? 1 : this.defaultOpacity))
+            .select(".markerCircle")
+            .attr("stroke-width", d => selectedSet.has(d.index) ? 2 : 0);
     }
 
     updateAxis = function(visData,xAttribute,yAttribute){
-        // compute min max using d3.min/max(visData.map(item=>item.attribute))
-        const minXAxis = d3.min(visData.map((item)=>{return item[xAttribute]}));
-        const maxXAxis = d3.max(visData.map((item)=>{return item[xAttribute]}));
-        const minYAxis = d3.min(visData.map((item)=>{return item[yAttribute]}));
-        const maxYAxis = d3.max(visData.map((item)=>{return item[yAttribute]}));
+        const minXAxis = d3.min(visData.map((item)=>{return +item[xAttribute]}));
+        const maxXAxis = d3.max(visData.map((item)=>{return +item[xAttribute]}));
+        const minYAxis = d3.min(visData.map((item)=>{return +item[yAttribute]}));
+        const maxYAxis = d3.max(visData.map((item)=>{return +item[yAttribute]}));
 
-        this.xScale.domain([minXAxis,maxXAxis]);
-        this.yScale.domain([minYAxis,maxYAxis]);
+        // fallback if all equal or undefined
+        this.xScale.domain([minXAxis === undefined ? 0 : minXAxis, maxXAxis === undefined ? 1 : maxXAxis]);
+        this.yScale.domain([minYAxis === undefined ? 0 : minYAxis, maxYAxis === undefined ? 1 : maxYAxis]);
 
-        // create axis with computed scales
         this.matSvg.select(".xAxisG")
-            .transition().duration(500)
-            .call(d3.axisBottom(this.xScale))
-        ;
+            .transition().duration(200)
+            .call(d3.axisBottom(this.xScale));
+
         this.matSvg.select(".yAxisG")
-            .transition().duration(500)
-            .call(d3.axisLeft(this.yScale))
+            .transition().duration(200)
+            .call(d3.axisLeft(this.yScale));
     }
 
 
     renderScatterplot = function (visData, xAttribute, yAttribute, controllerMethods){
-        console.log("render scatterplot with a new data list ...")
-        // build the size scales and x,y axis
-        this.updateAxis(visData, xAttribute, yAttribute);
+        this.data = visData || [];
+        // update scales & axes
+        this.updateAxis(this.data, xAttribute, yAttribute);
 
-        this.matSvg.selectAll(".markerG")
-            // all elements with the class .markerG (empty the first time)
-            .data(visData,(itemData)=>itemData.index)
-            .join(
-                enter=>{
-                    // all data items to add:
-                    // doesn’exist in the select but exist in the new array
-                    const itemG=enter.append("g")
-                        .attr("class","markerG")
-                        .style("opacity",this.defaultOpacity)
-                        .on("click", (event,itemData)=>{
-                            controllerMethods.handleOnClick(itemData);
-                        })
-                    ;
-                    // render element as child of each element "g"
-                    itemG.append("circle")
-                        .attr("class","markerCircle")
-                        .attr("r",this.circleRadius)
-                        .attr("stroke","red")
-                    ;
-                    this.updateMarkers(itemG,xAttribute,yAttribute);
-                },
-                update=>{
-                    this.updateMarkers(update,xAttribute,yAttribute)
-                },
-                exit =>{
-                    exit.remove()
-                    ;
+        // JOIN markers
+        const join = this.pointsG.selectAll(".markerG")
+            .data(this.data, (itemData)=>itemData.index);
+
+        join.join(
+            enter => {
+                const g = enter.append("g")
+                    .attr("class", "markerG")
+                    .attr("transform", (d)=>"translate("+this.xScale(d[xAttribute])+","+this.yScale(d[yAttribute])+")")
+                    .style("opacity", this.defaultOpacity);
+
+                g.append("circle")
+                    .attr("class","markerCircle")
+                    .attr("r", this.circleRadius)
+                    .attr("fill","steelblue")
+                    .attr("stroke","red")
+                    .attr("stroke-width",0)
+                    .on("click", (event, d)=>{
+                        if(controllerMethods && typeof controllerMethods.handleOnClick === "function"){
+                            controllerMethods.handleOnClick(d);
+                        }
+                    })
+                    .on("mouseenter", (event, d)=>{
+                        if(controllerMethods && typeof controllerMethods.handleOnMouseEnter === "function"){
+                            controllerMethods.handleOnMouseEnter(d);
+                        }
+                    })
+                    .on("mouseleave", (event, d)=>{
+                        if(controllerMethods && typeof controllerMethods.handleOnMouseLeave === "function"){
+                            controllerMethods.handleOnMouseLeave(d);
+                        }
+                    });
+
+                return g;
+            },
+            update => {
+                this.updateMarkers(update, xAttribute, yAttribute);
+                return update;
+            },
+            exit => exit.remove()
+        );
+
+        // set up brush (2D)
+        const that = this;
+        const brush = d3.brush()
+            .extent([[0, 0], [this.width, this.height]])
+            .on("end", (event) => {
+                if (!event.selection) {
+                    // clear selection
+                    if (controllerMethods && typeof controllerMethods.updateSelectedItems === "function") {
+                        controllerMethods.updateSelectedItems([]);
+                    }
+                    return;
                 }
+                const [[x0, y0], [x1, y1]] = event.selection;
+                // convert pixels to data-space
+                const xMin = that.xScale.invert(x0);
+                const xMax = that.xScale.invert(x1);
+                // note: yScale range is [height,0] so invert accordingly
+                const yMax = that.yScale.invert(y0);
+                const yMin = that.yScale.invert(y1);
 
-            )
+                const selected = that.data.filter(d => {
+                    const xv = +d[xAttribute];
+                    const yv = +d[yAttribute];
+                    return xv >= xMin && xv <= xMax && yv >= yMin && yv <= yMax;
+                }).map(d => ({...d, selected:true}));
+
+                if (controllerMethods && typeof controllerMethods.updateSelectedItems === "function") {
+                    controllerMethods.updateSelectedItems(selected);
+                }
+            });
+
+        // ensure only one brush attached
+        this.brushG.call(brush);
     }
 
     clear = function(){
--- src/components/scatterplot/ScatterplotContainer.js
+++ src/components/scatterplot/ScatterplotContainer.js
@@ -16,19 +16,16 @@ function ScatterplotContainer({scatterplotData, xAttribute, yAttribute, selected
     const scatterplotD3Ref = useRef(null)
 
     const getChartSize = function(){
-        // getting size from parent item
-        let width;// = 800;
-        let height;// = 100;
-        if(divContainerRef.current!==undefined){
-            width=divContainerRef.current.offsetWidth;
-            height=divContainerRef.current.offsetHeight-4;
+        let width = 800;
+        let height = 100;
+        if(divContainerRef.current){
+            width = divContainerRef.current.offsetWidth;
+            height = Math.max(120, divContainerRef.current.offsetHeight - 4);
         }
         return {width:width,height:height};
     }
 
-    // did mount called once the component did mount
     useEffect(()=>{
-        console.log("ScatterplotContainer useEffect [] called once the component did mount");
         const scatterplotD3 = new ScatterplotD3(divContainerRef.current);
         scatterplotD3.create({size:getChartSize()});
         scatterplotD3Ref.current = scatterplotD3;
@@ -52,24 +49,25 @@ function ScatterplotContainer({scatterplotData, xAttribute, yAttribute, selected
         }
         const handleOnMouseEnter = function(itemData){
         }
-        const handleOnMouseLeave = function(){
-        }
+        const handleOnMouseLeave = function(){};
 
-        const controllerMethods={
+        const controllerMethods = {
             handleOnClick,
             handleOnMouseEnter,
-            handleOnMouseLeave
-        }
+            handleOnMouseLeave,
+            updateSelectedItems: scatterplotControllerMethods && typeof scatterplotControllerMethods.updateSelectedItems === "function"
+                ? scatterplotControllerMethods.updateSelectedItems
+                : ()=>{}
+        };
 
-        if(scatterplotDataRef.current !== scatterplotData) {
-            console.log("ScatterplotContainer useEffect with dependency when scatterplotData changes...");
-            // get the current instance of scatterplotD3 from the Ref object...
-            const scatterplotD3 = scatterplotD3Ref.current
-            // call renderScatterplot of ScatterplotD3...;
-            scatterplotD3.renderScatterplot(scatterplotData, xAttribute, yAttribute, controllerMethods);
-            scatterplotDataRef.current = scatterplotData;
-        }
-    },[scatterplotData, xAttribute, yAttribute, scatterplotControllerMethods]);// if dependencies, useEffect is called after each data update, in our case only scatterplotData changes.
+        const scatterplotD3 = scatterplotD3Ref.current;
+        if(!scatterplotD3) return;
+
+        // render always (scales & brush depend on attributes even if data ref is same)
+        scatterplotD3.renderScatterplot(scatterplotData, xAttribute, yAttribute, controllerMethods);
+        scatterplotDataRef.current = scatterplotData;
+
+    },[scatterplotData, xAttribute, yAttribute, scatterplotControllerMethods]);
 
 
     useEffect(()=>{
